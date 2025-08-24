11046a780a14da052eb656f9d5cf59701aa7ce28
Jyotishman pathak <jyotishmanpathak11@gmail.com>
2025-08-24 22:21:27 +0530

    analytics

--- components/theme/Main.tsx
+++ components/theme/Main.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from './theme-provider'
 import {NavbarMain} from '../landingPage/Navbar'
 import { Toaster } from 'sonner'
 import { SessionProvider } from 'next-auth/react'
-
+import { Analytics } from "@vercel/analytics/next"
 const Main = ({children}:{children: ReactNode}) => {
   return (
     <div className="">
@@ -19,6 +19,7 @@ const Main = ({children}:{children: ReactNode}) => {
 
   forcedTheme='dark'
 >
+  <Analytics/>
               <Toaster />
   {children}
         </ThemeProvider>
